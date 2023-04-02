18a6337115c8ec73a68d05936e588e2ede18a74e
Nikita Schneider <komesergey@gmail.com>
2023-04-02 12:45:28 +0300

    Initial Commit

---
+++ CPU-M/CPU_MApp.swift
@@ -0,0 +1,17 @@
+//
+//  CPU_MApp.swift
+//  CPU-M
+//
+//  Created by Nikita Schneider on 02.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CPU_MApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
